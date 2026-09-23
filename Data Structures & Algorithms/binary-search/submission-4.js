93315b8e4ba491b0765beba833fbcf25d5d61462
Mohammad Umar Ahmed <87543349+umar77805@users.noreply.github.com>
2026-09-23 23:38:24 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) return mid;
+            if (target < nums[mid]) right = mid - 1
+            else left = mid + 1
+        }
+
+        return -1;
+    }
+}
